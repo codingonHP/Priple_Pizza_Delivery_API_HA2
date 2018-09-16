128c428b076273f295dee72d3fed36f831f83ada
Vishal Anand <vishal_24_anand@hotmail.com>
2018-09-16 11:26:56 +0530

    Refactored application start logic. Application used to start from Application class itself which created a problem while unit testing Application class. Unit test would start the server which is not something that is expected from a unit test. Solution to this is to move the application start logic to separate file index.ts and update package.json file's script section.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { Application } from './app';
 import { Configuration } from './configuration/Configuration';
 import { AppServer } from './core/server/server';
 
-Configuration.getConfiguration().then((config) => {
-    const appServer = new AppServer(config);
-    new Application().start(appServer, config);
-});
+function startApplication() {
+    Configuration.getConfiguration().then((config) => {
+        const appServer = new AppServer(config);
+        new Application().start(appServer, config);
+    });
+}
+
+startApplication();
